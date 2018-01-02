7a2f4a72ed4c474760f2ce2557459aebb2feb016
Frédéric Menou <frederic.menou@fretlink.com>
2018-01-02 23:00:40 +0100

    Parse Stream Metadatas from Twitch API

--- src/TwitchParser.hs
+++ src/TwitchParser.hs
@@ -1,8 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module TwitchParser
     ( extractViewerCount
     ) where
 
-import           Data.ByteString (ByteString)
+import           Data.Aeson
+import           Data.ByteString.Lazy (ByteString)
 
 extractViewerCount :: ByteString -> Int
-extractViewerCount = const 0
+extractViewerCount json =
+  case parser json of
+    Just ms -> checkViewerCount (metadatas ms)
+    Nothing -> 0
+
+checkViewerCount :: [Metadata] -> Int
+checkViewerCount [] = 0
+checkViewerCount ms = viewerCount (head ms)
+
+parser :: ByteString -> Maybe Metadatas
+parser json = decode json
+
+data Metadatas = Metadatas { metadatas :: [Metadata] }
+
+instance FromJSON Metadatas where
+  parseJSON (Object o) = Metadatas <$> o .: "data"
+  parseJSON invalid = fail "unexpected"
+
+data Metadata =
+  Metadata
+    { viewerCount :: Int
+    }
+
+instance FromJSON Metadata where
+  parseJSON (Object o) = Metadata <$> o .: "viewer_count"
+  parseJSON invalid = fail "unexpected"
