35c644b6a34e5e8478a7c11fac495a7d6e0a9e51
Frédéric Menou <frederic.menou@fretlink.com>
2018-01-16 22:38:56 +0100

    Read filepath from args

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,20 @@ module Main where
 
 import           TwitchAPI          (fetchViewerCount)
 
-import           System.Environment (getEnv)
+import           System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  viewerCount <- fetchViewerCount "ptit_fred"
-  home <- getEnv "HOME"
-  writeFile (home ++ "/livecoding/viewer-count.txt") (humanViewerCount viewerCount)
+  args <- getArgs
+  case getOutputPath args of
+    Just path -> do
+                   viewerCount <- fetchViewerCount "ptit_fred"
+                   writeFile path (humanViewerCount viewerCount)
+    Nothing   -> putStrLn "Missing argument: output path filename"
+
+getOutputPath :: [String] -> Maybe FilePath
+getOutputPath (path : _) = Just path
+getOutputPath []         = Nothing
 
 humanViewerCount :: Maybe Int -> String
 humanViewerCount (Just i)
