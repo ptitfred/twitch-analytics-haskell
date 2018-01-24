43348404d359bddb0fcbb4b4a99a6088051a1cab
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-01-24 13:04:50 +0000

    Add: instance FromRow/ToRow for video type

--- src/Database.hs
+++ src/Database.hs
@@ -2,11 +2,14 @@
 
 module Database where
 
-import           Data.Text                  (Text)
-import           Database.PostgreSQL.Simple (ConnectInfo (..), Connection,
-                                             close, connect, execute, query_)
-
-import           System.Environment         (getEnv)
+import           Data.Text                          (Text)
+import           Database.PostgreSQL.Simple         (ConnectInfo (..),
+                                                     Connection, close, connect,
+                                                     execute, query_)
+import           Database.PostgreSQL.Simple.FromRow (FromRow (..), field)
+import           Database.PostgreSQL.Simple.ToField (ToField (..), toField)
+import           Database.PostgreSQL.Simple.ToRow   (ToRow (..))
+import           System.Environment                 (getEnv)
 
 type URL = String
 
@@ -15,6 +18,12 @@ data Video =
         , title       :: Text
         , description :: Text
         } deriving Show
+
+instance ToRow Video where
+  toRow (Video u t d) = [toField u, toField t,  toField d]
+
+instance FromRow Video where
+  fromRow = Video <$> field <*> field <*> field
 
 insertVideo :: Video -> IO Bool
 insertVideo video = do
